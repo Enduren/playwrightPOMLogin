0237fcfd073880e1e3e7b0883787c3960da8fd05
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2025-12-15 18:30:09 -0600

    update automation

--- pages/addCoursesPage.js
+++ pages/addCoursesPage.js
@@ -5,7 +5,7 @@ class AddCourses {
         this.page = page
         this.addJava = "(//button[contains(text(),'Add to Cart')])[1]"
         this.addSelenium= "//div[@class='container-child']//div[2]//div[2]//button[1]"
-        this.addSubramaniK= "body > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2) > button:nth-child(6)"
+        this.addSubramaniK= `//*[@id="root"]/div/div[2]/div/div/div[3]/div[2]/button`
         
     }
 
