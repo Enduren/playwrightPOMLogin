b5440126b5f302ac25642eb174703cbac33c09fc
Dexter Dwayne Tennison <83023757+Enduren@users.noreply.github.com>
2025-12-16 19:35:56 -0600

    add new class for cart and called ir in main file

--- pages/cartPage.js
+++ pages/cartPage.js
@@ -3,6 +3,19 @@ const {expect}=require("@playwright/test")
 
 class CartPage {
     constructor(page) {
+        this.page = page
+        this.cartMenu = `:text-is("Cart")`
+        this.totalPriceH1 = "//div[@class='container-child']//h3[1]"
+    }
+
+    async clickCart() {
+        await this.page.click(this.cartMenu);
+        
+    }
+
+    async checkCart() {
+        
+        await expect(this.page.locator(this.totalPriceH1)).toBeVisible()
         
     }
 }
--- tests/loginApp.spec.js
+++ tests/loginApp.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 const LoginPage= require("../pages/loginpage")
 const HomePage= require("../pages/homePage")
 const AddCourses= require("../pages/addCoursesPage")
+const CartPage = require("../pages/cartPage")
 
 
 
@@ -12,19 +13,30 @@ test('Login To Application Using POM', async ({ page }) => {
     const loginPage = new LoginPage(page)
     const homePage= new HomePage(page)
     const addCourses= new AddCourses(page)
+    const cartPage = new CartPage(page)
+
 
     await loginPage.loginToApp("admin@email.com","admin@123")
 
     
-    homePage.verifyManageOption()
+    await homePage.verifyManageOption()
 
     await page.waitForTimeout(3000)
 
-    addCourses.addItems()
+    await addCourses.addItems()
+
+    await page.waitForTimeout(3000)
+
+    await cartPage.clickCart()
+
+    await page.waitForTimeout(3000)
+
+    await cartPage.checkCart()
 
     await page.waitForTimeout(3000)
     
     await homePage.logoutFromApp()
+
     await page.pause()
     
     
